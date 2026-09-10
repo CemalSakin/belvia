a08c07e70338b71a8b1574665d8e702bc71bb49f
Tahsin Sakin <tahcemdiscord@gmail.com>
2026-09-10 15:26:50 +0300

    Drop old Schedule shot; Example now schedule-ex.jpeg

--- ui.js
+++ ui.js
@@ -1,7 +1,7 @@
 const $ = (id) => document.getElementById(id);
 const EX = {
   trip: "./IMG_0554.jpeg",
-  schedule: "./IMG_0555.jpeg",
+  schedule: "./schedule-ex.jpeg",
   tickets: "./IMG_0556.jpeg",
   routes: "./IMG_0557.jpeg",
   bag: "./IMG_0559.jpeg",
