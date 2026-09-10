b6f891d20a25d9e5018f48dbc042769cab0210aa
Tahsin Sakin <tahcemdiscord@gmail.com>
2026-09-10 14:13:42 +0300

    Wire Tickets example to its own photo chunks

--- ex_tix.js
+++ ex_tix.js
@@ -1,1 +1,1 @@
-window.EX_TIX="data:image/jpeg;base64,"+(window._T1||"")+(window._T2||"");
+window.EX_TIX="data:image/jpeg;base64,"+(window._TX1||"")+(window._TX2||"")+(window._TX3||"")+(window._TX4||"")+(window._TX5||"")+(window._TX6||"")+(window._TX7||"");
