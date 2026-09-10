5beb02be6779ed08dabc7f63f715dd3ae29a66c6
Tahsin Sakin <tahcemdiscord@gmail.com>
2026-09-10 16:29:50 +0300

    Network-first SW; drop cache lock that served stale JS

--- sw.js
+++ sw.js
@@ -1,30 +1,13 @@
-const CACHE = "budvia-home-1";
-const PRECACHE = [
-  "./",
-  "./index.html",
-  "./manifest.webmanifest",
-  "./app.js",
-  "./ui.js",
-  "./pages.js",
-  "./events.js",
-  "./icon.js",
-  "./brand.js",
-  "./b1.js",
-  "./b2.js",
-  "./b3.js",
-  "./install.js"
-];
+const CACHE = "budvia-mit-1";
 
 self.addEventListener("install", function(event){
-  event.waitUntil(
-    caches.open(CACHE).then(function(cache){ return cache.addAll(PRECACHE); }).then(function(){ return self.skipWaiting(); })
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", function(event){
   event.waitUntil(
     caches.keys().then(function(keys){
-      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+      return Promise.all(keys.map(function(k){ return caches.delete(k); }));
     }).then(function(){ return self.clients.claim(); })
   );
 });
@@ -34,14 +17,11 @@ self.addEventListener("fetch", function(event){
   var url = new URL(event.request.url);
   if(url.origin !== self.location.origin) return;
   event.respondWith(
-    caches.match(event.request).then(function(hit){
-      if(hit) return hit;
-      return fetch(event.request).then(function(res){
-        var copy = res.clone();
-        caches.open(CACHE).then(function(cache){ cache.put(event.request, copy); }).catch(function(){});
-        return res;
-      }).catch(function(){
-        return caches.match("./index.html");
+    fetch(event.request).then(function(res){
+      return res;
+    }).catch(function(){
+      return caches.match(event.request).then(function(hit){
+        return hit || caches.match("./index.html");
       });
     })
   );
